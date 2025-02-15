4c605dc549120167a9d0512d5cbbf8a1785e1244
JackA1ltman <cs2dtzq@163.com>
2025-02-15 20:39:30 +0800

    moded

--- compile_mode.sh
+++ compile_mode.sh
@@ -9,18 +9,18 @@ fi
 IMAGE_DIR="$(pwd)/out/arch/arm64/boot"
 TIME=$(date +"%Y%m%d%H%M%S")
 CURRENT_FOLDER=$(basename "$(pwd)")
-USER_HOME=/home/jackaltman
+USER_HOME=/home/jackaltman/Kernel_Compile
 
 # Compiler Var
 CLANG_SELECT=los_clang_12
-PATH=$USER_HOME/Kernel_Compile/Clang/$CLANG_SELECT/bin:$PATH
-GCC_64=CROSS_COMPILE=$USER_HOME/Kernel_Compile/Gcc/gcc-64/bin/aarch64-linux-android-
-GCC_32=CROSS_COMPILE_ARM32=$USER_HOME/Kernel_Compile/Gcc/gcc-32/bin/arm-linux-androideabi-
+PATH=$USER_HOME/Clang/$CLANG_SELECT/bin:$PATH
+GCC_64=CROSS_COMPILE=$USER_HOME/Gcc/gcc-64/bin/aarch64-linux-android-
+GCC_32=CROSS_COMPILE_ARM32=$USER_HOME/Gcc/gcc-32/bin/arm-linux-androideabi-
 CLANG_OTHER=CLANG_TRIPLE=aarch64-linux-gnu-
 COMPILER_OPTION=LD=ld.lld
 COMPILER_ARCH=ARCH=arm64
 COMPILER_OUT=O=out
-COMPILER_DEFCONFIG=vendor/kona-perf_defconfig
+COMPILER_DEFCONFIG=custom-polaris_defconfig
 
 build(){
 make mrproper && rm -f error.log
@@ -29,6 +29,9 @@ make -j$(nproc --all) CC="ccache clang" $COMPILER_OUT $COMPILER_ARCH $COMPILER_O
 }
 
 anykernel3(){
+# AnyKernel3 Var
+AK3_LOCATION="$(pwd)/Anykernel3"
+
 mkdir -p tmp
 cp -fp $IMAGE_DIR/Image.gz tmp
 cp -fp $IMAGE_DIR/dtbo.img tmp
@@ -38,7 +41,7 @@ if [ -f "$IMAGE_DIR/dtb" ]; then
 else
 	echo "Doesn't found DTB! u device maybe needn't the file."
 fi
-cp -rp Anykernel3/* tmp
+cp -rp $AK3_LOCATION/* tmp
 cd tmp
 7za a -mx9 tmp.zip *
 cd ..
@@ -48,10 +51,26 @@ rm -rf tmp
 }
 
 mkbootimg(){
+# MKBOOTIMG Var
+FORMAT_MKBOOTIMG=$(echo `$USER_HOME/mkbootimg/unpack_bootimg.py --boot_img=$USER_HOME/KernelSourceIMGs/boot_$CURRENT_FOLDER.img --out outKernel --format mkbootimg`)
+IMAGE_MKBOOTIMG=Image.gz-dtb
+
 if [ $? -eq 0 ]; then
-        sed -i 's/^ROM_CURRENT=[^ ]*/ROM_CURRENT='$CURRENT_FOLDER'/' $USER_HOME/Kernel_Compile/generate.sh
-        cd $USER_HOME/Kernel_Compile/
-        bash generate.sh
+        if [ -f "outKernel" ]; then
+        	mkdir outKernel
+        	echo "Folder exist!"
+        fi
+        rm -f outKernel/*
+        $USER_HOME/mkbootimg/unpack_bootimg.py --boot_img $USER_HOME/KernelSourceIMGs/boot_$CURRENT_FOLDER.img --out outKernel
+	cp $IMAGE_DIR/$IMAGE_MKBOOTIMG outKernel/kernel
+	$USER_HOME/mkbootimg/mkbootimg.py --kernel outKernel/kernel --ramdisk outKernel/ramdisk $FORMAT_MKBOOTING -o boot_${CURRENT_FOLDER}_${TIME}.img
+	echo "========================"
+	echo "=                      ="
+	echo "=   Exec the command   ="
+	echo "=                      ="
+	echo "========================"
+	#echo "mkbootimg/mkbootimg.py $FORMAT_MKBOOTIMG -o /home/jackaltman/Kernel_Compile/Compiled_Kernel/boot_${CURRENT_FOLDER}_${TIME}.img"
+	echo "========================"
         echo "= Compile Successfully ="
         echo "========================"
 else
--- susfs.sh
+++ susfs.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
-ln -s susfs4ksu/kernel_patches/fs/* fs/
-ln -s susfs4ksu/kernel_patches/include/linux/* include/linux/
+#!/bin/bash
+$GIT_VERSION=kernel-4.19
+$GIT_LINK="https://gitlab.com/simonpunk/susfs4ksu.git"
+$KSU_NAME=KernelSU
+git clone -b $KERNEL_VERSION $GIT_LINK susfs4ksu
+cp susfs4ksu/kernel_patches/fs/* fs/
+cp susfs4ksu/kernel_patches/include/linux/* include/linux/
+cp susfs4ksu/kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch ./
+cp susfs4ksu/kernel_patches/50_add_susfs_in_$GIT_VERSION.patch ./
+cd $KSU_NAME
+patch -p1 < 10_enable_susfs_for_ksu.patch
+cd ../
+patch -p1 < 50_add_susfs_in_$GIT_VERSION.patch
